2a91797dddd2cb831e19d85fd5a194f947563ab9
cdelahoze <cdelahoze@hotmail.com>
2022-04-29 15:44:00 -0500

    correct the mistakes and resubmit

--- js/js.js
+++ js/js.js
@@ -2,3 +2,5 @@
 // set current year in footer
 const currentDate = new Date();
 document.querySelector('#year').textContent = currentDate.getFullYear();
+const upDate = new Date();
+document.querySelector('#update').textContent = upDate;
