74a03eaa783efd7de140df77c708e3033e16cbd8
cdelahoze <cdelahoze@hotmail.com>
2022-04-30 21:40:22 -0500

    add lesson 2

---
+++ lesson2/js/lesson2.js
@@ -0,0 +1,5 @@
+// set current year in footer
+const currentDate = new Date();
+document.querySelector('#year').textContent = currentDate.getFullYear();
+const upDate = new Date();
+document.querySelector('#update').textContent = upDate;
