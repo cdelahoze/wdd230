6f7b06a3f1219c5e7b76492ca5921b630cba2872
cdelahoze <cdelahoze@hotmail.com>
2022-06-21 21:14:03 -0500

    W07 ✅ Assignment: The Discover Page - Image Section

--- chamber/js/js.js
+++ chamber/js/js.js
@@ -40,3 +40,35 @@ if (dia.getDay() == 4) {
     document.querySelector('#banner').style.display="block";
 }
 }
+
+// lazy load efect
+
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = { threshold: 1, rootMargin: "0px 0px 50px 0px"};
+
+const loadImages = (img) => {
+  img.setAttribute('src', img.getAttribute('data-src'));
+  img.onload = () => {
+    img.removeAttribute('data-src');
+  };
+};
+
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    }, imgOptions) ;
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  } 
